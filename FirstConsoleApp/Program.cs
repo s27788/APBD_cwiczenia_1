65fbf2275a7e7a42d2a29cca893d4beae2c41fca
s27788 <s27788@pjwstk.edu.pl>
2025-03-02 14:53:40 +0100

    Feature average

--- FirstConsoleApp/Program.cs
+++ FirstConsoleApp/Program.cs
@@ -1,4 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System;
+
 Console.WriteLine("Modyfikacja 1: Cześć, World! :) ");
 Console.WriteLine("Modyfikacja 2: Co tam słychać?");
-Console.WriteLine("Modyfikacja 3: What's up? );
+Console.WriteLine("Modyfikacja 3: What's up?" );
+
+
+class Program
+{
+	static void Main()
+	{
+		double a = 2, b = 4, c = 6;
+		double average = (a + b + c) / 3;
+		Console.WriteLine($"Średnia: {average}");
+	}
+}
