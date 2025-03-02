49ecee231c55a072601ae877ce3fe772b5247860
s27788 <s27788@pjwstk.edu.pl>
2025-03-02 15:11:21 +0100

    fix class declaration

--- FirstConsoleApp/Program.cs
+++ FirstConsoleApp/Program.cs
@@ -2,15 +2,14 @@
 
 Console.WriteLine("Modyfikacja 1: Cześć, World! :) ");
 Console.WriteLine("Modyfikacja 2: Co tam słychać?");
-Console.WriteLine("Modyfikacja 3: What's up?" );
-
+Console.WriteLine("Modyfikacja 3: What's up?");
 
 class Program
 {
-	static void Main()
-	{
-		double a = 2, b = 4, c = 6;
-		double average = (a + b + c) / 3;
-		Console.WriteLine($"Średnia: {average}");
-	}
+    static void Main()
+    {
+        double a = 2, b = 4, c = 6;
+        double average = (a + b + c) / 3;
+        Console.WriteLine($"Średnia: {average}");
+    }
 }
