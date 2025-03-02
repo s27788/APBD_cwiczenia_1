6d4fa419b861c4e8628a3e74eb7565a72f24cf91
s27788 <s27788@pjwstk.edu.pl>
2025-03-02 13:43:51 +0100

    Modyfikacja 3

--- FirstConsoleApp/Program.cs
+++ FirstConsoleApp/Program.cs
@@ -1,3 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Modyfikacja 1: Cześć, World! :) ");
 Console.WriteLine("Modyfikacja 2: Co tam słychać?");
+Console.WriteLine("Modyfikacja 3: What's up? );
