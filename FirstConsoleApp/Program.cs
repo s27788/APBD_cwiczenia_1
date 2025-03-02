54e27e6d137ced16ce475b064100a4f5f042f689
s27788 <s27788@pjwstk.edu.pl>
2025-03-02 15:26:55 +0100

    feature max implementation

--- FirstConsoleApp/Program.cs
+++ FirstConsoleApp/Program.cs
@@ -12,5 +12,24 @@ class Program
         double a = 2, b = 4, c = 6;
         double average = (a + b + c) / 3;
         Console.WriteLine($"Średnia: {average}");
-    }
+    
+	// New method -> feature-max
+	int[] numbers = { 5, 10, 20, 100, 15, 25};
+	int maxValue = FindMaxValue(numbers);
+	Console.WriteLine($"Maksymalna wartość: {maxValue});
+	}
+
+	static int FindMaxValue(int[] numbers)
+	{
+		int max = numbers[0]; // indeks 0 jako pocz. wartość
+		foreach (int num in numbers)
+		{
+			if (num > max)
+			{
+				max = num; // jeśli istnieje większa, nadpisz
+			}
+		}
+	return max;
+	}
+}
 }
